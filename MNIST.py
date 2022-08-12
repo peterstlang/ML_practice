8248aa26e777962a340a65fb6dfa64ea50f58adf
peterstlang <peter.chriss@hotmail.com>
2022-08-12 05:29:21 +0200

    Update MNIST.py
    still wont work

--- MNIST.py
+++ MNIST.py
@@ -4,3 +4,11 @@ Created on Thu Aug 11 23:06:35 2022
 
 @author: peter
 """
+
+#import 
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
