9ae6d400c6bce510b382c2150bd72ba26512a614
peterstlang <peter.chriss@hotmail.com>
2022-08-24 21:58:18 +0200

    changed loading of the dataset

--- MNIST_data.py
+++ MNIST_data.py
@@ -16,73 +16,30 @@ import tensorflow_datasets as tfds
 
 #%%
 
-def load_mnist():
-    (ds_train, ds_test), ds_info = tfds.load(
-        'mnist',
-        split=['train', 'test'],
-        shuffle_files=True,
-        as_supervised=True,
-        with_info=True,
-        )
-    return ds_train, ds_test, ds_info
-
-def normalize_img(image, label):
-    """Normalizes images: `uint8` -> `float32`."""
-    return tf.cast(image, tf.float32) / 255., label
-
-def transform_MNIST_train(ds_train, ds_info):
-    ds_train = ds_train.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    ds_train = ds_train.cache()
-    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-    ds_train = ds_train.batch(128)
-    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
-    return ds_train
+def load_dataset(dataset='mnist'):
     
-def transform_MNIST_test(ds_test):
-    ds_test = ds_test.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    ds_test = ds_test.batch(128)
-    ds_test = ds_test.cache()
-    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
-    return ds_test
-
-def create_compile_model():
-    model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-    ])
+    if dataset in tfds.list_builders():
+        builder = tfds.builder(dataset)
+        builder.download_and_prepare()
+        ds = builder.as_dataset(split='train', shuffle_files=True)
+        return ds
+    else:
+        print("{} is not a valid dataset!".format(dataset))
+        
+def iterate_examples(ds=ds, num_examples=3):
+    ds = ds.take(num_examples)
     
-    model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
-    )
+    for example in ds:
+        print(list(example.keys()))
+        image = example["image"]
+        label = example["label"]
+        print(image.shape, label)
     
-    return model
-
-
-def fit_model(model, ds_train, ds_test):
-    return model.fit(
-    ds_train,
-    epochs=6,
-    validation_data=ds_test,
-    )
-
-def train_model(ds_train, ds_test):
-    model = create_compile_model()
-    model = fit_model(model, ds_train, ds_test)
-    return model
-    
-
-def splits():
-    ds_train, ds_test, ds_info = load_mnist()
-    ds_train = transform_MNIST_train(ds_train, ds_info)
-    ds_test = transform_MNIST_test(ds_test)
-    
-    
-    return ds_train, ds_test
 
 if __name__ == "__main__":
-    splits()
+
+    ds = load_dataset()
+    iterate_examples(num_examples=10)
+    
+    
+    
--- MNIST_model.py
+++ MNIST_model.py
@@ -4,11 +4,57 @@ Created on Mon Aug 15 23:19:55 2022
 
 @author: peter
 """
-from MNIST_data import splits
+import tensorflow as tf
+import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
+#%%
+# Old MNIST_data.py
+# trying to rework the old MNIST_data to better visualise and 
+# understand the dataset
+def load_mnist():
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+        )
+    return ds_train, ds_test, ds_info
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+def transform_MNIST_train(ds_train, ds_info):
+    ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+    return ds_train
+    
+def transform_MNIST_test(ds_test):
+    ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+    return ds_test
+
+
+def splits():
+    ds_train, ds_test, ds_info = load_mnist()
+    ds_train = transform_MNIST_train(ds_train, ds_info)
+    ds_test = transform_MNIST_test(ds_test)
+    
+    
+    return ds_train, ds_test
+
+#%%
 
 def create_compile_model():
     model = tf.keras.models.Sequential([
